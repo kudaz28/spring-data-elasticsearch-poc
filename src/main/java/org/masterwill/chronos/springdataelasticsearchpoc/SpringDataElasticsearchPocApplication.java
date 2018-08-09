475dc189614fc5268254461944ace7b616d7d6b9
William Zhou <williamkzhou@hotmail.com>
2018-08-09 20:29:20 +0100

    Initial commit of Spring Boot project on Elasticsearch

---
+++ src/main/java/org/masterwill/chronos/springdataelasticsearchpoc/SpringDataElasticsearchPocApplication.java
@@ -0,0 +1,12 @@
+package org.masterwill.chronos.springdataelasticsearchpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataElasticsearchPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataElasticsearchPocApplication.class, args);
+	}
+}
